bca79d6f79f79f4d04f6f82d786c18702423d82a
xAsiimov <admin@xasiimov.com>
2019-01-24 20:37:19 +0800

    Build Neural Style Transfer Backend with TensorFlow
    Signed-off-by: xAsiimov <admin@xasiimov.com>

---
+++ Backend/Image.py
@@ -0,0 +1,84 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+from PIL import Image
+
+import tensorflow as tf
+from tensorflow.python.keras.preprocessing import image as kp_image
+
+mpl.rcParams['figure.figsize'] = (10, 10)
+mpl.rcParams['axes.grid'] = False
+plt.figure(figsize=(10, 10))
+
+
+def load_img(path_to_img):
+    max_dim = 512
+    img = Image.open(path_to_img)
+    long = max(img.size)
+    scale = max_dim / long
+    img = img.resize((round(img.size[0] * scale), round(img.size[1] * scale)), Image.ANTIALIAS)
+
+    img = kp_image.img_to_array(img)
+
+    # We need to broadcast the image array such that it has a batch dimension
+    img = np.expand_dims(img, axis=0)
+    return img
+
+
+def imshow(img, title=None):
+    # Remove the batch dimension
+    out = np.squeeze(img, axis=0)
+    # Normalize for display
+    out = out.astype('uint8')
+    if title is not None:
+        plt.title(title)
+    plt.imshow(out)
+
+
+def load_and_process_img(path_to_img):
+    img = load_img(path_to_img)
+    img = tf.keras.applications.vgg19.preprocess_input(img)
+    return img
+
+
+def deprocess_img(processed_img):
+    x = processed_img.copy()
+    if len(x.shape) == 4:
+        x = np.squeeze(x, 0)
+    assert len(x.shape) == 3, ("Input to deprocess image must be an image of "
+                               "dimension [1, height, width, channel] or [height, width, channel]")
+    if len(x.shape) != 3:
+        raise ValueError("Invalid input to deprocessing image")
+
+    # perform the inverse of the preprocessiing step
+    x[:, :, 0] += 103.939
+    x[:, :, 1] += 116.779
+    x[:, :, 2] += 123.68
+    x = x[:, :, ::-1]
+
+    x = np.clip(x, 0, 255).astype('uint8')
+    return x
+
+
+def show_results(best_img, content_path, style_path, show_large_final=True):
+    plt.figure(figsize=(10, 5))
+    content = load_img(content_path)
+    style = load_img(style_path)
+
+    plt.subplot(1, 2, 1)
+    imshow(content, 'Content Image')
+
+    plt.subplot(1, 2, 2)
+    imshow(style, 'Style Image')
+
+    if show_large_final:
+        plt.figure(figsize=(10, 10))
+
+        plt.imshow(best_img)
+        plt.title('Output Image')
+        plt.show()
+
+
+def save_results(best_img):
+    img = Image.fromarray(best_img)
+    img.save('output/output.jpg')
---
+++ Backend/Main.py
@@ -0,0 +1,46 @@
+import Model
+import Image
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+import tensorflow as tf
+
+mpl.rcParams['figure.figsize'] = (10, 10)
+mpl.rcParams['axes.grid'] = False
+
+plt.figure(figsize=(10, 10))
+
+tf.enable_eager_execution()
+
+content_path = 'input/content.jpg'
+style_path = 'input/style.jpg'
+
+content = Image.load_img(content_path).astype('uint8')
+style = Image.load_img(style_path).astype('uint8')
+
+plt.subplot(1, 2, 1)
+Image.imshow(content, 'Content Image')
+
+plt.subplot(1, 2, 2)
+Image.imshow(style, 'Style Image')
+
+plt.show()
+
+# Content layer where will pull our feature maps
+content_layers = ['block5_conv2']
+
+# Style layer we are interested in
+style_layers = ['block1_conv1',
+                'block2_conv1',
+                'block3_conv1',
+                'block4_conv1',
+                'block5_conv1'
+                ]
+
+best, best_loss = Model.run_style_transfer(content_path,
+                                           style_path, num_iterations=1000)
+
+Image.show_results(best, content_path, style_path)
+
+Image.save_results(best)
